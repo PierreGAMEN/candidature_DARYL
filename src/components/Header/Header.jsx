1b5208ce88af673d0df581f710f65826095501d8
Pierre GAMEN <pierre.gamen@gmail.com>
2024-06-10 14:46:28 +0200

    add (Header): desktop element + style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,31 @@ import "./header.scss";
 
 const Header = () => {
   return (
-    <div>
-      <div/>
-    </div>
+    <header>
+      <div>
+        <img
+          src="https://static.wixstatic.com/media/9193e3_392cc0c1c8b248bd9023e599b969aa3b~mv2.png/v1/fill/w_353,h_111,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/daryl-logo.png"
+          alt="logo site DARYL social software"
+        />
+      </div>
+      <nav>
+        <a href="#">Accueil</a>
+        <a href="#">Nos solutions</a>
+        <a href="#">A propos</a>
+        <a href="#">Contact</a>
+        <div>
+          <div className="container-language">
+            <img
+              src="https://static.parastorage.com/services/linguist-flags/1.663.0/assets/flags/corner/FRA.png"
+              alt="drapeau français"
+            />
+            <button className="material-symbols-rounded">
+              keyboard_arrow_down
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
