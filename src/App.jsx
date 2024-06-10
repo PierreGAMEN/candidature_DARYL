6b181cffcc48438164986cff6857cc6e2efd391a
Pierre GAMEN <pierre.gamen@gmail.com>
2024-06-10 14:45:37 +0200

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
 // import { useState } from 'react'
 
-import './App.css'
-import About from './components/About/About'
-import Excerpt from './components/Excerpt/Excerpt'
-import Header from './components/Header/Header'
-import Mission from './components/Mission/Mission'
-import Nav from './components/Nav/Nav'
-import Solution from './components/Solution/Solution'
+import "./App.css";
+import About from "./components/About/About";
+import Excerpt from "./components/Excerpt/Excerpt";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Mission from "./components/Mission/Mission";
+import Nav from "./components/Nav/Nav";
+import Solution from "./components/Solution/Solution";
 
 function App() {
-
-
   return (
     <>
-    <Header />
-    <main>
-      <Excerpt />
-      <Mission />
-      <Solution />
-      <About />
-      <Nav />
-    </main>
- 
+      <Header />
+      <main>
+        <Hero />
+        <Excerpt />
+        <Mission />
+        <Solution />
+        <About />
+        <Nav />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
