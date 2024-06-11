f23ce3a14dd5440d8472489149f7ecde9d500495
Pierre GAMEN <pierre.gamen@gmail.com>
2024-06-11 10:44:55 +0200

    add: Hero component + style

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,10 @@ import "./hero.scss";
 
 const Hero = () => {
   return (
-    <section>
-      
+    <section className="container_hero">
+      <h1>Développeur fullstack</h1>
+      <p>Regardez ce site comme une lettre de motivation personnalisée.</p>
+      <a href="https://pierre-gamen.netlify.app/">En savoir plus</a>
     </section>
   );
 };
