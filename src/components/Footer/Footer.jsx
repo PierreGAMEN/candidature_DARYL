389132d64145c129030b2daeff002afef1cbd771
Pierre GAMEN <pierre.gamen@gmail.com>
2024-06-13 11:07:58 +0200

    add: link in the footer to reach DarylSoftware website

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./footer.scss";
 const Footer = () => {
   return (
     <footer>
-      <p>Site original : DARYL SOFTWARE.</p>
+      <p>Site original : <a href="https://www.darylsoftware.com/">DARYL SOFTWARE.</a></p>
       <p>Réinterprété par : Pierre GAMEN.</p>
       <p>Réalisé avec React, Vite et déployé avec Netlify.</p>
     </footer>
